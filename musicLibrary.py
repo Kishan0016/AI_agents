6031dba517948e5e5077ec9f281a7cf69469993a
Kishan Yadav <kishanyadav5987@gmail.com>
2026-05-20 09:55:21 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    'song' : "https://youtu.be/zSapKpmOcWs?si=bUYy6dMi135Ioa9p",
+    'tension': "https://youtu.be/bgGH0wcjUOA?si=t9A0oPql0ZYrJQOB",
+    'sister': "https://youtu.be/_5c6EmJurxc?si=b3LArQpEAWMllZUJ"
+}
